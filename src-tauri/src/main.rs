65f225b22daa5d0cbb84992708c358c4eae55e59
TienNguyen <tuantiensiu@gmail.com>
2025-01-07 16:30:20 +0700

    add: actix-web serve

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use actix_files::Files;
+use actix_web::{App, HttpServer};
+use std::thread;
 
-fn main() {
-    tauri_app_lib::run()
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Start Actix web server in a separate thread
+    thread::spawn(|| {
+        let sys = actix_web::rt::System::new();
+        sys.block_on(async {
+            HttpServer::new(|| {
+                App::new().service(Files::new("/", "/Users/tien/Documents").show_files_listing())
+            })
+            .bind("127.0.0.1:3030")
+            .unwrap()
+            .run()
+            .await
+        })
+    });
+
+    // Run Tauri in the main thread
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+    Ok(())
 }
